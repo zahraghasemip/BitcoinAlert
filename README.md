9b542956a055d47a30059f8591022b4216ec35ba
Zahra Ghasemi <zahraghasemi2487@gmail.com>
2024-08-03 09:44:55 +0330

    alert added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # BitcoinAlert
+
+**Installation**
+
+To get started, you'll need to install a couple of Python packages. You can do this using pip. Open your terminal and run the following commands:
+
+```bash
+# Install cryptocompare
+$ pip3 install cryptocompare
+
+# Install pygame
+$ pip install pygame
+```
+`cryptocompare`: This package is used for accessing cryptocurrency data.
+`pygame`: This package is used for playing MP3 files.
+
--- btc_price.py
+++ btc_price.py
@@ -1,11 +1,19 @@
 import cryptocompare
 from time import sleep
+from pygame import mixer
+mixer.init()
+
+
 while True:
     btc_price = cryptocompare.get_price('BTC', currency='USD')["BTC"]["USD"]
     if btc_price < 60000 :
         print("btc went low", btc_price)
+        mixer.music.load ( "data/beep.mp3" )
+        mixer.music.play()
     elif btc_price > 60100 :
         print("btc went high", btc_price)
+        mixer.music.load( "data/beep.mp3" )
+        mixer.music.play()
     else:
         print(btc_price)
     sleep(7)
